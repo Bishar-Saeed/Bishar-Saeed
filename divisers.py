4e64f9300f328f50e5cd257e6a01f8ed4bbe3b3d
Bishar-Saeed <bisharchh@gmail.com>
2024-06-04 19:40:48 +0500

    DIVISERS_FIND

---
+++ divisers.py
@@ -0,0 +1,8 @@
+cmd="non-divisible"
+for i in range(0,10):
+     if cmd=="divisible":
+         if i%2==0:
+             print(i)
+     if cmd=="non-divisible":
+         if i%2!=0:
+             print(i)
